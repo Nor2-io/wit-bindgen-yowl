6ced57eabbb92396d01da1b6684d1fac445b3acd
yowl00 <scott.waye@hubse.com>
2023-08-06 21:25:19 -0500

    start to remove mono specific files

--- crates/csharp/tests/codegen.rs
+++ crates/csharp/tests/codegen.rs
@@ -1,2 +1,22 @@
 //TODO: Implement tests similar to the other generators.
 // This requires that we have any dependencies either included here or published to NuGet or similar.
+
+macro_rules! codegen_test {
+    ($id:ident $name:tt $test:tt) => {
+        #[test]
+        fn $id() {
+            test_helpers::run_world_codegen_test(
+                "guest-csharp",
+                $test.as_ref(),
+                |resolve, world, files| {
+                    wit_bindgen_csharp::Opts {
+                        generate_stub: true,
+                    }
+                    .build()
+                    .generate(resolve, world, files)
+                },
+            )
+        }
+    };
+}
+test_helpers::codegen_tests!();
