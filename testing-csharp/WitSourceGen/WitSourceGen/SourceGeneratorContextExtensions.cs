77be997ff78702bce994f05c68acd4fca305f6d7
yowl00 <scott.waye@hubse.com>
2023-08-17 19:39:30 -0500

    add source generator

---
+++ testing-csharp/WitSourceGen/WitSourceGen/SourceGeneratorContextExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Linq;
+
+namespace WitSourceGen
+{
+    internal static class SourceGeneratorContextExtensions
+    {
+        private const string SourceItemGroupMetadata = "build_metadata.AdditionalFiles.SourceItemGroup";
+
+        public static string GetMsBuildProperty(
+            this GeneratorExecutionContext context,
+            string name,
+            string defaultValue = "")
+        {
+
+
+            context.AnalyzerConfigOptions.GlobalOptions.TryGetValue($"build_property.{name.ToLowerInvariant()}", out var value);
+            return value ?? defaultValue;
+        }
+
+        public static string[] GetMsBuildItems(this GeneratorExecutionContext context, string name)
+            => context
+                .AdditionalFiles
+                .Where(f => context.AnalyzerConfigOptions
+                                .GetOptions(f)
+                                .TryGetValue(SourceItemGroupMetadata, out var sourceItemGroup)
+                            && sourceItemGroup == name)
+                .Select(f => f.Path)
+                .ToArray();
+    }
+
+}
